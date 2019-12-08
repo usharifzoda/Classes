766d1975a97c062a96369a6017e881932cafa32d
usharifzoda <sharifzoda.umedjoni@gmail.com>
2019-12-07 22:32:52 -0500

    First Python Commit

---
+++ ClassAndObjects.py
@@ -0,0 +1,10 @@
+class Tuna:
+
+    def __init__(self):
+        print('ahahahaha')
+
+    def swim(self):
+        print('I am swimming')
+
+flipper = Tuna()
+flipper.swim()
---
+++ Genders.py
@@ -0,0 +1,18 @@
+import Enemy
+
+class Girl:
+
+    gender = 'female'
+
+    def __init__(self, name):
+        self.name = name
+
+r = Girl('Rachel')
+s = Girl('Stanky')
+
+
+
+print(r.name)
+print(s.name)
+
+print(r.gender)
